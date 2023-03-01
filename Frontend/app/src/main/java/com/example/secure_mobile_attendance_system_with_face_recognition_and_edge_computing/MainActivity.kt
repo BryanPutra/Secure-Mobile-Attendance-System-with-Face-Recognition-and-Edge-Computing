4f6a6fb1e9864ea032c43556dd2fa6495268b4c1
BryanPutra <bpsuandi@gmail.com>
2023-03-01 23:11:56 +0700

    setup android studio

---
+++ Frontend/app/src/main/java/com/example/secure_mobile_attendance_system_with_face_recognition_and_edge_computing/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.secure_mobile_attendance_system_with_face_recognition_and_edge_computing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
